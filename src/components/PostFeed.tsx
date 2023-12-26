fcd06402c876ec51167e9472b79c22a3279d635b
Raphael Chevallier <chevallierr@vmware.com>
2023-12-26 15:59:36 -0800

    minor

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -71,7 +71,7 @@ const PostFeed: FC<PostFeedProps> = ({ initialPosts, region, regionType, latitud
     <ul className='flex flex-col col-span-2 space-y-6'>
       {posts.length === 0 && region === 'CUSTOM FEED' ? "No posts on your feed yet. Subscribe to regions with existing posts." : null}
       {posts.length === 0 && region != 'CUSTOM FEED' && region != 'GENERAL FEED' ? "This region does not contain any posts yet. Be the first to post for here!" : null}
-      {posts.length === 0 && region === 'GENERAL FEED' ? "No posts have been made at all yet. Be the first to post a listing!" : null}
+      {posts.length === 0 && region === 'GENERAL FEED' ? "No posts have been made yet. Be the first to post a listing!" : null}
       {posts.map((post, index) => {
         const votesAmt = post.votes.reduce((acc, vote) => {
           if (vote.type === 'UP') return acc + 1
